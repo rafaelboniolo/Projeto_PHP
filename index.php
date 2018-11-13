4b7a37a54469dcb78be4f2556331ee1d7e93b05c
Rafael Boniolo <boniolo@alunos.utfpr.edu.br>
2018-11-13 09:07:43 -0200

    testes rotas

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
     
     $app = new Router();
 
-    $app->route('/insert', function($json){
-        echo $json;
+
+    $app->route('/insert', function(){
+        $json = file_get_contents('php://input'); // json de entrada
+        print_r($json);
+
+        $resultado = json_decode($json,true); // conversão do json para array associativo
+        print_r($resultado);
+
+        http_response_code(404); // status de retorno da requisição
+
     });
 
-    $app->route('/update', function(){
-
+    $app->route('/update', function(){ // rota para acessar dados
+        
     });
 
     $app->route('/delete', function(){
@@ -26,6 +34,21 @@
     $app->route('/findall', function(){
 
     });
+
+    $app->route('/retornaGraficos', function(){
+
+
+    
+        $json['class'] = "Pessoa";
+        $json['nome']="Rafael";
+        $json['cpf']="673254";
+        
+        
+        $json1 = json_encode($json); // transformar array associativo para json
+        print_r($json1);
+    });
+
+    
 
 
     $app->run();
